e661dcbc212510e61c055a185c2d8dd0a0a6ba70
The_Valkyrie <110331611+Kirieshek@users.noreply.github.com>
2024-02-10 01:48:46 +0300

    Add files via upload

---
+++ homework/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <Check :tasks="tasks"  @addTask="addTask" @deleteTask="deleteTask" @editTask="editTask"/>
+</template>
+
+<script>
+import Check from './components/Check.vue'
+
+export default {
+  name: 'App',
+  components:{
+    Check
+  },
+data(){
+  return{
+    tasks:[
+      {id: 1, text: 'Помыться', isEdit: false, isEdit2: false,},
+      {id: 2, text: 'Покушать', isEdit: false, isEdit2: false,},
+      {id: 3, text: 'Убраться', isEdit: false, isEdit2: false,},
+      {id: 4, text: 'Прогуляться', isEdit: false, isEdit2: false,},
+      {id: 5, text: 'Поваляться', isEdit: false, isEdit2: false,},
+    ],
+   }
+}
+,
+methods:{
+      addTask(newText){
+        this.tasks.unshift({
+          id: Date.now(),
+          text: newText,
+          isEdit2: false,
+        })
+      },
+      deleteTask(id) {
+      this.tasks = this.tasks.filter(tasks => tasks.id !== id)
+      },
+      editTask(task) {
+      task.isEdit2 = !task.isEdit2
+    },
+    },
+  }
+
+
+</script>
+
+<style>
+
+</style>
---
+++ homework/src/components/Check.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <input v-model="newText" type="text">
+        <button @click="addTask">Добавить</button>
+
+            <ul>
+                <li v-for="task in tasks" :key="task.id" :style="{ 'text-decoration': task.isEdit ? 'line-through' : 'none' }">
+                
+
+                <template v-if="task.isEdit2 == true">
+                <input type="text" v-model="task.text   ">    
+                <button @click="editTask(task)">Сохранить</button>
+                <button @click="deleteAff(task.id)">Удалить</button>
+                </template>
+
+                <template v-else>
+                {{ task.text }}
+                <input type="checkbox" v-model="task.isEdit">
+                <button @click="editTask(task) ">Изменить</button>
+                <button @click="deleteAff(task.id)">Удалить</button>
+                </template>
+                </li>
+            </ul>
+
+    </div>
+</template>
+<script>
+export default {
+    name: 'Check',
+    props:{
+        tasks: Object,
+        newText: String,
+    },
+    emits:['addTask', 'deleteTask', 'editTask'],
+
+    data(){
+        return{
+            newText: '',
+        }
+    },
+    methods:{
+        deleteAff(id){
+            this.$emit('deleteTask', id)
+        },
+        addTask(){
+            this.$emit('addTask', this.newText)
+        },
+        editTask(task){
+            this.$emit('editTask', task)
+        },
+    },
+    
+}
+</script>
+<style>
+
+</style>
